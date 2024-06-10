bb2985f14b63eaea295386bdaeabbeb5f92cf75c
Victória Amaral <ttoiav.aamaral@gmail.com>
2024-06-10 00:40:50 -0300

    data-science(analise-de-dados): Mantendo código limpo.

--- IntroducaoDataScience/AnaliseDados/TratandoDados.py
+++ IntroducaoDataScience/AnaliseDados/TratandoDados.py
@@ -1,32 +1,40 @@
+# Importando a biblioteca pandas para manipulação de dados
 import pandas as pd
+
+# Importando o pacote openpyxl para permitir a leitura de arquivos Excel
 #!pip install openpyxl
 
-# Criando conexão com o arquivo xsls;
+# Criando conexão com o arquivo Excel
 df = pd.read_excel('São_paulo,xsls')
 
+# Selecionando uma linha específica do DataFrame (linha com índice 100031)
 auxiliar = df.loc[100031]
 
-#Criando Função limpa_preco;
+# Criando a função limpa_preco para limpar e formatar os valores na coluna 'price'
 def limpa_preco(linha):
-    linha['price'] = linha['price'].replace('R$', '')
-    linha['price'] = linha['price'].replace('\n', '')
-    linha['price'] = linha['price'].replace('/Mês ', '')
-    linha['price'] = linha['price'].replace('           ', '.')
+    linha['price'] = linha['price'].replace('R$', '')  # Remove o símbolo de Real
+    linha['price'] = linha['price'].replace('\n', '')  # Remove quebras de linha
+    linha['price'] = linha['price'].replace('/Mês ', '')  # Remove a indicação de preço mensal
+    linha['price'] = linha['price'].replace('           ', '.')  # Substitui múltiplos espaços por um ponto
     return linha
 
-df.apply(lambda x: limpa_preco(x), axis=1) # axis=1 = eixo 1, vulgo linhas!
+# Aplicando a função limpa_preco em cada linha do DataFrame
+df.apply(lambda x: limpa_preco(x), axis=1)  # axis=1 indica que a aplicação é feita por linha
 
+# Criando a função ajusta_alugueis para ajustar os valores de aluguel na coluna 'price'
 def ajusta_alugueis(linha):
-    linha['price'] = linha['price'].replace('.', '')
-    preco = int(linha['price'])
-    if(int(linha['price'])) < 10000:
-        preco = int(linha['price']) * 200
-    linha['price'] = preco
+    linha['price'] = linha['price'].replace('.', '')  # Remove pontos dos valores
+    preco = int(linha['price'])  # Converte o valor para inteiro
+    if preco < 10000:  # Se o valor for menor que 10.000
+        preco = preco * 200  # Multiplica o valor por 200
+    linha['price'] = preco  # Atualiza o valor na coluna 'price'
     return linha
 
+# Aplicando a função ajusta_alugueis na linha auxiliar
 ajusta_alugueis(auxiliar)
 
+# Aplicando a função ajusta_alugueis em cada linha do DataFrame
 df.apply(lambda x: ajusta_alugueis(x), axis=1)
 
-
+# Selecionando novamente a linha específica do DataFrame para verificar as alterações
 auxiliar = df.loc[100031]
